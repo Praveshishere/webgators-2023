19bac1da47f8fa2f0891c4f082e3069431541286
vinoddotcom <vinod.yadav@vidyamantra.com>
2023-03-31 14:44:27 +0530

    Added player

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex w-full h-screen justify-center items-start bg-[#0F172A] dark">
+    <div class="w-[700px] mt-12">
+    <Sidebar />
+  </div>
   </div>
 </template>
+<script setup lang="ts">
+// import Sidebar from "@/components/sidebar.vue"
+import Sidebar from "@/components/player.vue"
+</script>
